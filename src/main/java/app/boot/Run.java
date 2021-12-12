4e0404cb3c8bc5af97e756e42dc02ab6081ef5df
matan <matan33214@gmail.com>
2021-12-12 20:43:41 +0200

    conflict

--- src/main/java/app/boot/Run.java
+++ src/main/java/app/boot/Run.java
@@ -5,6 +5,7 @@ import app.editor.GlfwWindow;
 public class Run {
     public static void main(String[] args) {
         GlfwWindow glfwWindow = new GlfwWindow("OPENGL");
+
         glfwWindow.run();
     }
 
