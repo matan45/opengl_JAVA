cc1ee817562f011486fd9f1ed4362cc5580ee908
matan <matan33214@gmail.com>
2021-12-24 11:11:07 +0200

    add style to TransformComponent

--- src/main/java/app/ecs/components/TransformComponent.java
+++ src/main/java/app/ecs/components/TransformComponent.java
@@ -2,11 +2,13 @@ package app.ecs.components;
 
 import app.ecs.Component;
 import imgui.ImGui;
+import imgui.flag.ImGuiCol;
 
 public class TransformComponent implements Component {
 
-    float[] val = new float[1];
-    float[] val2 = new float[1];
+    float[] X = new float[1];
+    float[] Y = new float[1];
+    float[] Z = new float[1];
 
     @Override
     public void update(float dt) {
@@ -15,10 +17,30 @@ public class TransformComponent implements Component {
 
     @Override
     public void imguiDraw() {
-        ImGui.sliderFloat("x", val, 0.0f, 1.0f);
-        ImGui.sliderFloat("y", val2, 0.0f, 1.0f);
-        ImGui.labelText("", val[0] + "");
-        ImGui.labelText("", val2[0] + "");
+        ImGui.pushStyleColor(ImGuiCol.Button, 0.8f, 0.1f, 0.15f, 1.0f);
+        ImGui.pushStyleColor(ImGuiCol.ButtonHovered, 0.9f, 0.2f, 0.2f, 1.0f);
+        ImGui.pushStyleColor(ImGuiCol.ButtonActive, 0.8f, 0.1f, 0.15f, 1.0f);
+        ImGui.button("X");
+        ImGui.popStyleColor(3);
+        ImGui.sameLine();
+        ImGui.dragFloat("##X", X, 0.1f);
+
+        ImGui.pushStyleColor(ImGuiCol.Button, 0.1f, 0.8f, 0.15f, 1.0f);
+        ImGui.pushStyleColor(ImGuiCol.ButtonHovered, 0.2f, 0.9f, 0.2f, 1.0f);
+        ImGui.pushStyleColor(ImGuiCol.ButtonActive, 0.1f, 0.8f, 0.15f, 1.0f);
+        ImGui.button("Y");
+        ImGui.popStyleColor(3);
+        ImGui.sameLine();
+        ImGui.dragFloat("##Y", Y, 0.1f);
+
+        ImGui.pushStyleColor(ImGuiCol.Button, 0.15f, 0.1f, 0.8f, 1.0f);
+        ImGui.pushStyleColor(ImGuiCol.ButtonHovered, 0.1f, 0.2f, 0.9f, 1.0f);
+        ImGui.pushStyleColor(ImGuiCol.ButtonActive, 0.1f, 0.15f, 0.8f, 1.0f);
+        ImGui.button("Z");
+        ImGui.popStyleColor(3);
+        ImGui.sameLine();
+        ImGui.dragFloat("##Z", Z, 0.1f);
+
     }
 
     @Override
