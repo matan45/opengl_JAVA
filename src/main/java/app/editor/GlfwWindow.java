234648d805a69b968b6a7b11cfca8c6b5a20506c
matan <matan33214@gmail.com>
2022-01-22 12:15:39 +0200

    legacy functions

--- src/main/java/app/editor/GlfwWindow.java
+++ src/main/java/app/editor/GlfwWindow.java
@@ -58,8 +58,11 @@ public class GlfwWindow {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+        //for debug mode
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+        //for support legacy functions
+        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
         //window options
         glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
