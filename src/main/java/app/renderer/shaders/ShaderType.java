06170bfbf6d22dd147e64424be474c2c36aeee8f
matan <matan33214@gmail.com>
2022-02-02 15:15:33 +0200

    hdr refactor

--- src/main/java/app/renderer/shaders/ShaderType.java
+++ src/main/java/app/renderer/shaders/ShaderType.java
@@ -2,10 +2,14 @@ package app.renderer.shaders;
 
 import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
 import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
+import static org.lwjgl.opengl.GL32.GL_GEOMETRY_SHADER;
+import static org.lwjgl.opengl.GL43.GL_COMPUTE_SHADER;
 
 public enum ShaderType {
     VERTEX(GL_VERTEX_SHADER),
-    FRAGMENT(GL_FRAGMENT_SHADER);
+    FRAGMENT(GL_FRAGMENT_SHADER),
+    GEOMETRY(GL_GEOMETRY_SHADER),
+    COMPUTE(GL_COMPUTE_SHADER);
 
     private final int value;
     ShaderType(int value) { this.value = value; }
