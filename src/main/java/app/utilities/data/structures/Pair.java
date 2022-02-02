72c9e2cd606a04c224e84dc1f6222e2994f5e561
matan <matan33214@gmail.com>
2022-02-02 12:49:32 +0200

    add multi

---
+++ src/main/java/app/utilities/data/structures/Pair.java
@@ -0,0 +1,27 @@
+package app.utilities.data.structures;
+
+public class Pair <T,K>{
+    T value;
+    K value2;
+
+    public Pair(T value, K value2) {
+        this.value = value;
+        this.value2 = value2;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public K getValue2() {
+        return value2;
+    }
+
+    public void setValue2(K value2) {
+        this.value2 = value2;
+    }
+}
