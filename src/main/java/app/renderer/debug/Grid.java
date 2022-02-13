3ac573310cd2c15b7dd95b85589ce73e6199cc21
matan <matan33214@gmail.com>
2022-02-13 12:25:57 +0200

    grid

--- src/main/java/app/renderer/debug/Grid.java
+++ src/main/java/app/renderer/debug/Grid.java
@@ -76,10 +76,6 @@ public class Grid {
     }
 
     //TODO MOVE IT after mirage
-    public static int[] listIntToArray(List<Integer> list) {
-        return list.stream().mapToInt((Integer v) -> v).toArray();
-    }
-
     public static float[] listToArray(List<Float> list) {
         int size = list != null ? list.size() : 0;
         float[] floatArr = new float[size];
