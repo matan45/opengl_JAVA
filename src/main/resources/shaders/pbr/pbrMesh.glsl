e66d6092832d5bcb00811beb59456f0f8c3811f6
matan <matan33214@gmail.com>
2022-02-14 14:18:34 +0200

    material

--- src/main/resources/shaders/pbr/pbrMesh.glsl
+++ src/main/resources/shaders/pbr/pbrMesh.glsl
@@ -8,10 +8,16 @@ uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
 
+out vec2 TexCoords;
+out vec3 WorldPos;
+out vec3 Normal;
 
 void main()
 {
+    TexCoords = texcoords;
+    Normal = mat3(model) * normal;
     vec4 worldPosition = model * vec4(position, 1.0);
+    WorldPos = worldPosition.xyz;
 	vec4 positionRelativeToCam = view * worldPosition;
  	gl_Position = projection * positionRelativeToCam;
 }
@@ -19,6 +25,24 @@ void main()
 #type FRAGMENT
 #version 460 core
 out vec4 FragColor;
+
+in vec2 TexCoords;
+in vec3 WorldPos;
+in vec3 Normal;
+
+// material parameters
+uniform sampler2D albedoMap;
+uniform sampler2D normalMap;
+uniform sampler2D metallicMap;
+uniform sampler2D roughnessMap;
+uniform sampler2D aoMap;
+uniform sampler2D displacementMap;
+uniform sampler2D emissiveMap;
+
+// IBL
+uniform samplerCube irradianceMap;
+uniform samplerCube prefilterMap;
+uniform sampler2D brdfLUT;
 
 void main()
 {
