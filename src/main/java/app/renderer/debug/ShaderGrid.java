8886b095f42de8d9edcd71e4361d05e4f9adf5fe
matan <matan33214@gmail.com>
2022-02-19 16:22:49 +0200

    fix

--- src/main/java/app/renderer/debug/ShaderGrid.java
+++ src/main/java/app/renderer/debug/ShaderGrid.java
@@ -9,7 +9,6 @@ import java.nio.file.Path;
 public class ShaderGrid extends ShaderProgram {
     private int locationProjectionMatrix;
     private int locationViewMatrix;
-    private int locationModelMatrix;
 
     protected ShaderGrid(Path path) {
         super(path);
@@ -19,16 +18,11 @@ public class ShaderGrid extends ShaderProgram {
     protected void getAllUniformLocations() {
         locationProjectionMatrix = super.getUniformLocation(UniformsNames.PROJECTION.getUniformsName());
         locationViewMatrix = super.getUniformLocation(UniformsNames.VIEW.getUniformsName());
-        locationModelMatrix = super.getUniformLocation("model");
     }
 
 
     public void loadViewMatrix(OLMatrix4f view) {
         super.loadMatrix(locationViewMatrix, view);
-    }
-
-    public void loadModelMatrix(OLMatrix4f model) {
-        super.loadMatrix(locationModelMatrix, model);
     }
 
     public void loadProjectionMatrix(OLMatrix4f projection) {
