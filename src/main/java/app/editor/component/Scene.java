761f0372dd208a8ba71a4104bf03843c730979b9
matan <matan33214@gmail.com>
2022-04-13 14:31:39 +0300

    scene

---
+++ src/main/java/app/editor/component/Scene.java
@@ -0,0 +1,11 @@
+package app.editor.component;
+
+import app.ecs.Entity;
+
+import java.util.List;
+
+public class Scene {
+    private String name;
+    private String path;
+    private List<Entity> entities;
+}
