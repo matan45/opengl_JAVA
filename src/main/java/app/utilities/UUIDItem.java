5db7c0bc82f1479958c60a2faaf6f9c95f1d66a6
matan <matan33214@gmail.com>
2022-05-23 09:05:04 +0300

    uuid

---
+++ src/main/java/app/utilities/UUIDItem.java
@@ -0,0 +1,11 @@
+package app.utilities;
+
+import java.util.random.RandomGenerator;
+
+public class UUIDItem {
+    private static final RandomGenerator generator = RandomGenerator.of("L64X256MixRandom");
+
+    public static long generateUUID() {
+        return generator.nextLong(1, Long.MAX_VALUE);
+    }
+}
